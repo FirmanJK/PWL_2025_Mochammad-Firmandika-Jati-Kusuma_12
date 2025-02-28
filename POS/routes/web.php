55265757422ee1f6b1500c597d035ab1da276e27
FirmanJK <jatikusuma761@gmail.com>
2025-02-28 08:54:49 +0700

    Soal Praktikum_Penambahan Route

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/// Route untuk halaman Home
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Route prefix untuk halaman Products
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [CategoryController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [CategoryController::class, 'beautyHealth']);
+    Route::get('/home-care', [CategoryController::class, 'homeCare']);
+    Route::get('/baby-kid', [CategoryController::class, 'babyKid']);
+});
+
+// Route dengan parameter untuk halaman User
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Route untuk halaman Penjualan (POS)
+Route::get('/sales', [SalesController::class, 'index']);
